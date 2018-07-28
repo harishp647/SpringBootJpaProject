6e620bf215c70cd7a5f65cfab405c66aba53c649
harishp647 <harishp1647@gmail.com>
2018-07-27 23:14:45 -0700

    springboot

---
+++ SpringBootJpaProject/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into alien values(101,'navin','java');
+insert into alien values(102,'eavin','Android');
+insert into alien values(103,'Pravin','Python');
+insert into alien values(104,'Kamal','java');
+insert into alien values(105,'ccn','javaspring');
+insert into alien values(106,'navvccvin','cbncn');
